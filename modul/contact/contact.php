abc286c677fd422a65610a53c58f62ee2bc83da3
URINURCAHYANI17 <urinurcahyani@gmail.com>
2017-06-07 21:55:08 +0700

    good

---
+++ modul/contact/contact.php
@@ -0,0 +1,65 @@
+<div class="contact">
+						<h3>Contact</h3>
+						<div class="bor hidden-xs"></div>
+						<img src="img/peta.jpg">
+						<div class="contact-details">
+							<!-- Contact form  -->
+							<h4>Contact Us</h4>
+							<form role="form" class="form">
+								<div class="form-group">
+									<!-- Name input box -->
+									<label for="exampleInputEmail1">Name</label>
+									<input type="text" class="form-control" placeholder="Nama" title="Nama Tidak Boleh Kosong" id="nama" required pattern=".+" />
+								</div>
+								<div class="form-group">
+									<!-- Email input box -->
+									<label for="exampleInputPassword1">Email</label>
+									<input type="email" class="form-control" placeholder="Email" title="Format email asep@mail.com" id="email" required pattern=".+@.+.">
+								</div>
+								<div class="form-group">
+									<!-- Message input box -->
+									<label>Content</label>
+									<textarea class="form-control" rows="3"></textarea>
+								</div>
+								
+									<!-- Submit and reset button -->
+								<button type="submit" class="btn btn-default">Kirim</button>
+								<button type="reset" class="btn btn-default">Reset</button>
+							</form>
+							<!-- Address -->
+							<h5>Address</h5>
+							<p><span>AsOne Corporation</span><br />
+								Jeruk Kepek, Wonosari 55183<br />
+								Yogyakarta<br />
+								P: (085) 293-172-490</p>
+							<div class="c-social">
+								<a href=""><i class="icon-facebook"></i></a>
+								<a href=""><i class="icon-google-plus"></i></a>
+								<a href=""><i class="icon-twitter"></i></a>
+								<a href=""><i class="icon-linkedin"></i></a>
+							</div>
+							<div class="pull-right">
+								askfnaslfnkasfnasfkasfn</div>
+						<div class="clearfix"></div>
+					</div>
+			</div>		
+
+				<footer>
+					<div class="row">
+						<div class="col-md-12">
+							<hr />
+							<br />
+							<!-- You should not remove the footer link back. -->
+							<p class="text-center">Copyright 2020 - <i>AsOne Corporation</i> - Designed by <i>Asep Setyawan</i></p>
+							<br />
+						</div>
+					</div>
+				</footer>
+<span class="totop"><a href="#"><i class="icon-chevron-up"></i></a></span> 
+
+<script type="text/javascript">
+	$("#nama").tooltip();
+</script>
+<script type="text/javascript">
+	$("#email").tooltip();
+</script>
